63d51bfc0d7c3b8748f34f7b695494d6d309cabe
Brayan-724 <bjqf0724@gmail.com>
2021-05-20 17:59:05 -0500

    AutoCommited

--- src/routes/FileRoutes/Files/getAll.js
+++ src/routes/FileRoutes/Files/getAll.js
@@ -3,7 +3,7 @@ const CTRL = require("../../../models/file/controller");
 function noTokens(obj) {
 	if(obj.success) {
 		return {
-			seccess: true,
+			success: true,
 			data: obj.data.map((v) => {
 				v.tokens = "MR. HACKER";
 				v.file.data = Buffer.from("MR. HACKER");
